6bf6afeb9817b9bb5462dcc128c6239030b2a1e4
Abhishekvenky010 <abhishekvenky010@gmail.com>
2026-04-21 01:17:30 +0530

    initial setup

---
+++ components/WalletProvider.tsx
@@ -0,0 +1,22 @@
+"use client";
+import {FC, ReactNode, useMemo} from "react";
+import {ConnectionProvider, WalletProvider} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+type Props = {
+    children : ReactNode;
+};
+const WalletContextProvider : FC<Props> = ({children})=>{
+    const endpoint  = "https://api.devnet.solana.com";
+    const wallet = useMemo(()=>[new PhantomWalletAdapter()],[]);
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallet}>
+                <WalletModalProvider>{children}</WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
+export default WalletContextProvider;
